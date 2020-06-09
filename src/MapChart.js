0102970e74b14f06120e91c6a112782757d839ba
Casey Idzikowski <idzikoj1234@gmail.com>
2020-06-09 14:46:22 -0500

    Mapbox Map with useSuperCluster

--- src/MapChart.js
+++ src/MapChart.js
@@ -1,58 +1,162 @@
-import React, { Component } from "react";
-import axios from "axios";
-import {
-  ComposableMap,
-  Geographies,
-  Geography,
-  Sphere,
-  Marker,
-} from "react-simple-maps";
+import React, { useState, useEffect, useRef } from "react";
+import MapGl, { Marker, Popup, FlyToInterpolator } from "react-map-gl";
+import { Popover, Tooltip, Position, Button, Classes } from "@blueprintjs/core";
+import "./App.css";
+import "mapbox-gl/dist/mapbox-gl.css";
+import Tippy from "@tippy.js/react";
+import useSuperCluster from "use-supercluster";
 
-const markers = [];
+function MapChart() {
+  const [markers, setMarkers] = useState([]);
+  const [viewport, setViewport] = useState({
+    latitude: 45,
+    longitude: 75,
+    width: "100vw",
+    height: "100vh",
+    zoom: 1,
+  });
 
-const geoUrl =
-  "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+  const mapRef = useRef();
 
-class MapChart extends Component {
-  constructor(props) {
-    super(props);
+  const [selectedSample, setSelectedSample] = useState(null);
 
-    this.state = {
-      data: null,
-    };
-  }
+  useEffect(() => {
+    getMarkers();
+  }, []);
 
-  componentDidMount() {
-    axios
-      .get(`https://sparrow-data.org/labs/wiscar/api/v1/sample?all=1`)
-      .then((res) => {
-        const data = res.data;
-        this.setState({ data });
-      });
-  }
-
-  render() {
-    return (
-      <ComposableMap projectionConfig={{ scale: 147 }}>
-        <Sphere stroke="#FF5533" strokeWidth={2} />
-        <Geographies geography={geoUrl}>
-          {({ geographies }) =>
-            geographies.map((geo) => (
-              <Geography key={geo.rsmKey} geography={geo} />
-            ))
-          }
-        </Geographies>
-          {this.state.data.map(({name, coordinates}) => <Marker key={name} coordinates={coordinates}>
-          <circle r={10} fill="#F00" stroke="#fff" strokeWidth={2} />
-          </Marker>)}
-        {/* {markers.map(({ name, coordinates}) => (
-        <Marker key={name} coordinates={coordinates}>
-          <circle r={10} fill="#F00" stroke="#fff" strokeWidth={2} />
-        
-        </Marker> */}
-      </ComposableMap>
+  async function getMarkers() {
+    const response = await fetch(
+      "https://sparrow-data.org/labs/wiscar/api/v1/sample?all=1"
     );
+    const markers = await response.json();
+    setMarkers(markers);
   }
+
+  const mapMarkers = markers.filter((d) => d.geometry != null);
+
+  const points = mapMarkers.map((markers) => ({
+    type: "Feature",
+    properties: {
+      cluster: false,
+      id: markers.id,
+      Sample_name: markers.name,
+      project_name: markers.prpject_name,
+    },
+    geometry: {
+      type: "Point",
+      coordinates: [
+        markers.geometry.coordinates[0],
+        markers.geometry.coordinates[1],
+      ],
+    },
+  }));
+
+  // get map bounds using map ref
+  const bounds = mapRef.current
+    ? mapRef.current.getMap().getBounds().toArray().flat()
+    : null;
+
+  // get clusters
+  const { clusters, supercluster } = useSuperCluster({
+    points,
+    zoom: viewport.zoom,
+    bounds,
+    options: { radius: 75, maxZoom: 5 },
+  });
+
+  return (
+    <div>
+      <MapGl
+        {...viewport}
+        mapboxApiAccessToken={
+          "pk.eyJ1IjoiY2FzZXktaWR6IiwiYSI6ImNrYjcyZ3ZydzAxbDIycnBkemcycGx0am4ifQ.VXQmbOI-mi0zAXZlNqbRLw"
+        }
+        onViewportChange={(viewport) => {
+          setViewport(viewport);
+        }}
+        ref={mapRef}
+      >
+        {clusters.map((cluster) => {
+          const [longitude, latitude] = cluster.geometry.coordinates;
+          const {
+            cluster: isCluster,
+            point_count: pointCount,
+          } = cluster.properties;
+
+          if (isCluster) {
+            return (
+              <Marker
+                key={cluster.id}
+                longitude={longitude}
+                latitude={latitude}
+              >
+                <div
+                  className="cluster-marker"
+                  style={{
+                    width: `${10 + (pointCount / points.length) * 150}px`,
+                    height: `${10 + (pointCount / points.length) * 150}px`,
+                  }}
+                  onClick={() => {
+                    const expansionZoom = Math.min(
+                      supercluster.getClusterExpansionZoom(cluster.id),
+                      5
+                    );
+                    setViewport({
+                      ...viewport,
+                      longitude,
+                      latitude,
+                      zoom: expansionZoom,
+                      transitionInterpolator: new FlyToInterpolator({
+                        speed: 1,
+                      }),
+                      transitionDuration: "auto",
+                    });
+                  }}
+                >
+                  {pointCount}
+                </div>
+              </Marker>
+            );
+          }
+
+          return (
+            <Marker
+              key={cluster.properties.id}
+              latitude={latitude}
+              longitude={longitude}
+            >
+              <Tippy content={cluster.properties.Sample_name}>
+                <button
+                  className="mrker-btn"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setSelectedSample(cluster);
+                  }}
+                >
+                  <img src="/logo.svg" alt="Generic Icon" />
+                </button>
+              </Tippy>
+            </Marker>
+          );
+        })}
+
+        {selectedSample ? (
+          <Popup
+            latitude={selectedSample.geometry.coordinates[1]}
+            longitude={selectedSample.geometry.coordinates[0]}
+            onClose={() => {
+              setSelectedSample(null);
+            }}
+          >
+            <div>
+              <p> Sample Name: {selectedSample.properties.Sample_name}</p>
+              <p>{selectedSample.properties.project_name}</p>
+            </div>
+          </Popup>
+        ) : null}
+      </MapGl>
+    </div>
+  );
 }
 
 export default MapChart;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
-
+import ReactTooltip from "react-tooltip";
 import "./index.css";
 
 import MapChart from "./MapChart";
 
 function App() {
+  
   return (
     <div>
-      <MapChart />
+      <MapChart/>
     </div>
   );
 }
